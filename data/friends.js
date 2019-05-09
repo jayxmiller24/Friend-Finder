d43622acb291bf23ff20c10f74a95e27461f0a95
jxmiller5 <jxmiller5@gmail.com>
2019-05-08 22:37:06 -0700

    added all files, added friends.js, added server.js code

---
+++ data/friends.js
@@ -0,0 +1,84 @@
+let friends = [
+    {
+        name: "John Smith", 
+        photo: "https://images.app.goo.gl/ZtVCjV1mAju3HF5w6://images.app.goo.gl/ZtVCjV1mAju3HF5w6s://media.giphy.com/media/wQzqIYHE15zMI/giphy.gif",
+        scores: [
+            "1",
+            "1",
+            "5",
+            "2",
+            "5",
+            "4",
+            "5",
+            "1",
+            "5",
+            "3",
+        ]
+    },
+    {
+        name: "Michael Miller", 
+        photo: "https://images.app.goo.gl/ZtVCjV1mAju3HF5w6://images.app.goo.gl/ZtVCjV1mAju3HF5w6://media.giphy.com/media/13jghlUIB6FHZm/giphy.gif",
+        scores: [
+            "5",
+            "2",
+            "3",
+            "1",
+            "4",
+            "2",
+            "2",
+            "4",
+            "2",
+            "5",
+        ]
+    },
+    {
+        name: "Ron Levinson", 
+        photo: "https://media.gihttps://images.app.goo.gl/ZtVCjV1mAju3HF5w6phy.com/media/OLHy9ERaFUvzW/giphy.gif",
+        scores: [
+            "2",
+            "2",
+            "5",
+            "3",
+            "5",
+            "5",
+            "1",
+            "3",
+            "4",
+            "2",
+        ]
+    },
+    {
+        name: "John Malone", 
+        photo: "https://media.giphy.com/mhttps://images.app.goo.gl/ZtVCjV1mAju3HF5w6edia/Su7qfpu8YVBqE/giphy.gif",
+        scores: [
+            "3",
+            "1",
+            "2",
+            "5",
+            "1",
+            "3",
+            "5",
+            "2",
+            "3",
+            "2",
+        ]
+    },
+    {
+        name: "Sally Weasley", 
+        photo: "https://images.app.goo.gl/7cYvP3CEtWryGTRc7",
+        scores: [
+            "3",
+            "5",
+            "2",
+            "1",
+            "2",
+            "1",
+            "5",
+            "4",
+            "2",
+            "5",
+        ]
+    },
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,21 @@
+let express = require("express");
+let path = require("path");
+
+
+let app = express();
+
+
+let PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
