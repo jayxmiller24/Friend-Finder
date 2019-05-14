f5538399d118a80d5222a183206cc2a3a84d7edf
jxmiller5 <jxmiller5@gmail.com>
2019-05-14 13:20:14 -0700

    changed some code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Friend-Finder
+This is a dating app. This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+The survey will have 10 questions. Each answer should be on a scale of 1 to 5 based on how much the user agrees or disagrees with a question.
--- routing/apiroutes.js
+++ routing/apiroutes.js
@@ -8,22 +8,22 @@ module.exports = function(app) {
     app.post("/api/friends", function (req, res) {
 
 
-        var bestMatch = {
+        let bestMatch = {
             name: "",
             photo: "",
             friendDifference: 1000
         };
 
-        var userData = req.body;
-        var userScores = userData.scores;
+        let userData = req.body;
+        let userScores = userData.scores;
 
-        var totalDifference = 0;
+        let totalDifference = 0;
 
-        for (var i = 0; i < friends.length; i++) {
+        for (let i = 0; i < friends.length; i++) {
             console.log(friends[i].name);
             totalDifference = 0;
 
-            for (var j = 0; j < friends[i].scores[j]; j++) {
+            for (let j = 0; j < friends[i].scores[j]; j++) {
                 totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 
                 if (totalDifference <= bestMatch.friendDifference) {
